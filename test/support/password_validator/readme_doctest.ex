36628e56d6b980a8c9ae8681d018abe2ab096c3e
Jason Axelson <jason.axelson@gmail.com>
2019-09-02 13:27:05 -1000

    Fix absolute path

--- test/support/password_validator/readme_doctest.ex
+++ test/support/password_validator/readme_doctest.ex
@@ -2,6 +2,6 @@ defmodule PasswordValidator.ReadmeDoctest do
   @file "#{Path.join([File.cwd!(), "README.md"])}"
   @external_resource @file
   @moduledoc """
-  #{File.read!("/home/jason/dev/password_validator_zxcvbn/README.md")}
+  #{File.read!(@file)}
   """
 end
