9bb279820b20abcd43339bcac53adb87dd58366f
Jason Axelson <axelson@users.noreply.github.com>
2019-09-02 13:38:39 -1000

    Changes required for release (#2)

--- mix.exs
+++ mix.exs
@@ -1,12 +1,15 @@
 defmodule PasswordValidatorZxcvbn.MixProject do
   use Mix.Project
 
+  @app :password_validator_zxcvbn
   @version "0.1.0"
 
   def project do
     [
-      app: :password_validator_zxcvbn,
+      app: @app,
       version: @version,
+      description: description(),
+      package: package(),
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -19,6 +22,24 @@ defmodule PasswordValidatorZxcvbn.MixProject do
       source_url: "https://github.com/axelson/password-validator-zxcvbn",
       homepage_url: "https://github.com/axelson/password-validator-zxcvbn"
     ]
+  end
+
+  def package do
+    [
+      name: @app,
+      files: ["lib", "mix.exs", "README*", "LICENSE*", "Changelog.md"],
+      maintainers: ["Jason Axelson"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/axelson/password-validator-zxcvbn"}
+    ]
+  end
+
+  def description do
+    """
+    A library to validate passwords, with built-in validators for password
+    length as well as the character sets used. Custom validators can also be
+    created.
+    """
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
